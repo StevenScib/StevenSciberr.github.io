d57c1d4aa428526730ad94eb4c6596d052b22267
StevenScib <x23174668@student.ncirl.ie>
2023-11-28 13:55:10 +0000

    DONE

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
+let userGuesses = [];
+let gameCount = 0;
 var clickCount = 0;
-function check(playerChoice){
+
+function check(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * 3)];
 
+    userGuesses.push(playerChoice); 
+
     if (playerChoice === computerChoice) {
-        document.getElementById('winner').innerText = "It's a tie! :/ try again by clicking any choice again";
-    } 
-    
-    else if (playerChoice === 'rock') {
+        document.getElementById('result').innerText = "It's a tie!";
+    } else if (playerChoice === 'rock') {
         if (computerChoice === 'scissors') {
-            document.getElementById('winner').innerText = 'You win! :) Rock beats scissors, try again by clicking any choice again.';
+            document.getElementById('result').innerText = 'You win! Rock beats scissors.';
         } else {
-            document.getElementById('winner').innerText = 'You lose! :( Paper beats rock try again by clicking any choice again.';
-            disableButtons();
+            document.getElementById('result').innerText = 'You lose! Paper beats rock.';
         }
-    } 
-
-    else if (playerChoice === 'paper') {
+    } else if (playerChoice === 'paper') {
         if (computerChoice === 'rock') {
-            document.getElementById('winner').innerText = 'You win! :) Paper beats rock try again by clicking any choice again.';
+            document.getElementById('result').innerText = 'You win! Paper beats rock.';
         } else {
-            document.getElementById('winner').innerText = 'You lose! :( Scissors beats paper try again by clicking any choice again.';
-            disableButtons();
+            document.getElementById('result').innerText = 'You lose! Scissors beats paper.';
         }
-    } 
-
-    else if (playerChoice === 'scissors') {
+    } else if (playerChoice === 'scissors') {
         if (computerChoice === 'paper') {
-            document.getElementById('winner').innerText = 'You win! :) Scissors beats paper try again by clicking any choice again.';
+            document.getElementById('result').innerText = 'You win! Scissors beats paper.';
         } else {
-            document.getElementById('winner').innerText = 'You lose! :( Rock beats scissors try again by clicking any choice again.';
-            disableButtons();
+            document.getElementById('result').innerText = 'You lose! Rock beats scissors.';
         }
+    } else {
+        document.getElementById('result').innerText = 'Invalid choice. Please choose rock, paper, or scissors.';
     }
-        clickCount++;
+
+    gameCount++;
+
+    if (gameCount === 5) {
+        endGame();
+    }
+    clickCount++;
+    if (clickCount === 6) {
+        disableButtons();
+    } 
+}
+
+function endGame() {
+    document.getElementById('guesses').innerText += userGuesses.join(', ');
+    userGuesses = [];
+    gameCount = 0;
+}
+
+clickCount++;
         function disableButtons() {
             var buttons = document.getElementsByTagName('button');
             for (var i = 0; i < buttons.length; i++) {
                 buttons[i].disabled = true;
             }
         }
-        if (clickCount === 3) {
-            disableButtons();
-        } 
-}
